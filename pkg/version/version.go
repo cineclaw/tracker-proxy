2d720a137c96602ee69c0fc421d28e3a697396c0
Andrey Viktorov <andvikt@gmail.com>
2026-09-14 07:51:14 +0300

    chore(release): bump version to 1.2.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 // Version holds the current semantic version of tracker-proxy.
 // It can be overridden at build time via -ldflags="-X 'github.com/cineclaw/tracker-proxy/pkg/version.Version=v1.0.0'"
 var (
-	Version   = "2.0.0"
+	Version   = "1.2.0"
 	BuildTime = ""
 	Commit    = ""
 )
